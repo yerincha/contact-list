8bbcf43b71298692cf8a8aed18ebb29b9266c4c8
Yerin Cha <lenacha32@gmail.com>
2022-07-28 16:56:53 -0700

    Implement form for editing contact and add handleChange function to support changes for each inputs

--- src/pages/edit/index.jsx
+++ src/pages/edit/index.jsx
@@ -1,20 +1,47 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-export default function Edit(props) {
+export default function Edit({ contact }) {
+  const [editedContact, setEditedContact] = useState({
+    firstName: contact.firstName,
+    lastName: contact.lastName,
+    phone: contact.phone,
+    email: contact.email,
+  });
+
+  function handleChange(e) {
+    const { value } = e.target;
+    setEditedContact({
+      ...editedContact,
+      [e.target.name]: value,
+    });
+  }
+
   const navigate = useNavigate();
-  const handleContentClick = (e, item) => {
-    e.preventDefault();
-    props.setSelectedContact(item);
-    navigate('/edit');
-  };
 
   return (
-    <div key={props.contact.id}>
-      <div>{props.contact.firstName}</div>
-      <div>{props.contact.lastName}</div>
-      <div>{props.contact.phone}</div>
-      <div>{props.contact.email}</div>
-    </div>
+    <form>
+      <label>
+        First name
+        <input
+          type="text"
+          name="firstName"
+          value={editedContact.firstName}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Last name
+        <input type="text" name="lastName" value={editedContact.lastName} onChange={handleChange} />
+      </label>
+      <label>
+        Phone
+        <input type="text" name="phone" value={editedContact.phone} onChange={handleChange} />
+      </label>
+      <label>
+        Email
+        <input type="text" name="email" value={editedContact.email} onChange={handleChange} />
+      </label>
+    </form>
   );
 }
