2f595cc2168b5200215fe9b7820ef5939e9bddf0
Yerin Cha <lenacha32@gmail.com>
2022-07-29 12:07:20 -0700

    Implement handleDelete functions for deleting contact

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,18 +1,41 @@
+import {confirmAlert} from 'react-confirm-alert';
 import React, {useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import ContactList from '../../components/ContactList';
-
-export default function Home({list, setSelectedContact}) {
+import Modal from '../../components/Modal';
+export default function Home({list, setList, contact, setSelectedContact}) {
   const navigate = useNavigate();
+  const [openDeleteModal, setOpenDeleteModal] = useState(false);
   const handleCreateClick = (e) => {
     e.preventDefault();
     navigate('/create');
   };
+  const handleOpenDeleteModal = () => {
+    setOpenDeleteModal(true);
+  };
 
+  const handleDeleteTrue = () => {
+    if (openDeleteModal) {
+      let filteredList = list.filter((item) => item.id !== contact.id);
+      setList(filteredList);
+      setOpenDeleteModal(false);
+    }
+  };
+
+  const handleDeleteFalse = () => {
+    setOpenDeleteModal(false);
+  };
   return (
     <>
+      {openDeleteModal && (
+        <Modal handleDeleteTrue={handleDeleteTrue} handleDeleteFalse={handleDeleteFalse} />
+      )}
       <button onClick={handleCreateClick}>Create New Contact</button>
-      <ContactList list={list} setSelectedContact={setSelectedContact} />
+      <ContactList
+        list={list}
+        setSelectedContact={setSelectedContact}
+        handleOpenDeleteModal={handleOpenDeleteModal}
+      />
     </>
   );
 }
