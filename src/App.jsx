c903ce64ff3f1d336f1424d72f41a65b588b6998
Yerin Cha <lenacha32@gmail.com>
2022-07-29 08:50:02 -0700

    Add route for routing to '/create' page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, {useState} from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import mockContactData from './data/mockData';
 
 import Home from './pages/home';
 import Edit from './pages/edit';
+import Create from './pages/create';
 
 function App() {
   const [list, setList] = useState(mockContactData);
@@ -15,7 +16,14 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home list={list} setSelectedContact={setSelectedContact} />} />
-          <Route path="/edit" element={<Edit contact={seletedContact} />} />
+          <Route
+            path="/edit"
+            element={<Edit list={list} contact={seletedContact} setList={setList} />}
+          />
+          <Route
+            path="/create"
+            element={<Create list={list} setList={setList} />}
+          />
         </Routes>
       </Router>
     </div>
