4adf575d55c23f7d6a99bf00dd12bc92a0f038c9
Yerin Cha <lenacha32@gmail.com>
2022-07-29 12:43:56 -0700

    Fix the file extension from .js to .jsx

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,10 +1,14 @@
+import React from 'react';
+
 export default function Modal({handleDeleteTrue, handleDeleteFalse}) {
   return (
     <div className="modal">
       <div className="modal_box">
         <p>You sure you wanna delete?</p>
-        <button className="modal_buttonCancel" onClick={handleDeleteFalse}>Cancel</button>
-        <button onClick={handleDeleteTrue} className="modal_buttoDelete">
+        <button type="button" className="modal_buttonCancel" onClick={handleDeleteFalse}>
+          Cancel
+        </button>
+        <button type="button" onClick={handleDeleteTrue} className="modal_buttoDelete">
           Confirm
         </button>
       </div>
