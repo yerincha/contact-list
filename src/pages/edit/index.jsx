848b1eeb70c7deaef87b79a8ba1240a7300e78e6
Yerin Cha <lenacha32@gmail.com>
2022-08-02 15:46:42 -0700

    Add callback that updates editedContact using useEffect hook when user changes the values of the inputs

--- src/pages/edit/index.jsx
+++ src/pages/edit/index.jsx
@@ -1,9 +1,10 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {useNavigate} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 export default function Edit({list, contact, setList}) {
   const [editedContact, setEditedContact] = useState({...contact});
+  const [emptyInput, setEmptyInput] = useState(true);
 
   const navigate = useNavigate();
 
@@ -34,32 +35,66 @@ export default function Edit({list, contact, setList}) {
     });
   };
 
+  useEffect(() => {
+    const {firstName, lastName, email, phone} = editedContact;
+    if (!firstName.length || !lastName.length || !phone.length || !email.length) {
+      setEmptyInput(true);
+    } else {
+      setEmptyInput(false);
+    }
+  }, [editedContact]);
+
   return (
     <form onSubmit={handleSubmit}>
+      <h1>Edit Existing Contact</h1>
       <label htmlFor="a">
         First name
         <input
           type="text"
           name="firstName"
+          aria-labelledby="firstName"
           value={editedContact.firstName}
           onChange={handleChange}
         />
       </label>
       <label htmlFor="a">
         Last name
-        <input type="text" name="lastName" value={editedContact.lastName} onChange={handleChange} />
+        <input
+          type="text"
+          name="lastName"
+          aria-labelledby="lastName"
+          value={editedContact.lastName}
+          onChange={handleChange}
+        />
       </label>
       <label htmlFor="a">
         Phone
-        <input type="text" name="phone" value={editedContact.phone} onChange={handleChange} />
+        <input
+          type="text"
+          name="phone"
+          aria-labelledby="phone"
+          value={editedContact.phone}
+          onChange={handleChange}
+        />
       </label>
       <label htmlFor="a">
         Email
-        <input type="text" name="email" value={editedContact.email} onChange={handleChange} />
+        <input
+          type="text"
+          name="email"
+          aria-labelledby="email"
+          value={editedContact.email}
+          onChange={handleChange}
+        />
       </label>
-      <input type="submit" value="Submit" />
-      <input type="button" value="Cancel" onClick={handleCancelClick} />
-      <input type="button" value="Go Back To The List" onClick={() => navigate('/')} />
+      <input type="submit" aria-labelledby="Submit" value="Submit" disabled={emptyInput} />
+      <input type="button" aria-labelledby="Cancel" value="Cancel" onClick={handleCancelClick} />
+      <input
+        type="button"
+        aria-labelledby="Go Back To The List"
+        value="Go Back To The List"
+        onClick={() => navigate('/')}
+      />
     </form>
   );
 }
